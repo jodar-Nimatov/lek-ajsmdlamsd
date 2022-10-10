3190b632b182d7d3025ee25b0ca5bdeb53bc3a0f
Jodar <zodarbekn@gmail.com>
2022-10-10 18:08:20 +0600

    dz-1

---
+++ main.js
@@ -0,0 +1,14 @@
+const userName = prompt("Введите имя")
+const userLastname = prompt("Введите фамилие")
+
+alert("Здраствуйте " + userName)
+console.log("Здраствуйте " + userName);
+
+/*------2------*/
+
+const num1 = ("Введите первое число")
+const num2 = ("Введите второе число")
+
+if(num1 < num2)
+
+
